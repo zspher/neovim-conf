96d7280fd718de5a963b9f3e0e905580221e5777
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-11-25 01:42:16 +0800

    style: use 2 spaces

--- lua/plugins/linter-formatter.lua
+++ lua/plugins/linter-formatter.lua
@@ -1,29 +1,29 @@
 return {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, config)
-        local null_ls = require "null-ls"
+  "nvimtools/none-ls.nvim",
+  opts = function(_, config)
+    local null_ls = require "null-ls"
 
-        -- Check supported formatters and linters
-        -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-        -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-        config.sources = {
-            null_ls.builtins.formatting.shfmt.with { extra_args = { "-i", vim.o.tabstop, "-ci" } },
-            null_ls.builtins.formatting.prettierd.with {
-                filetypes = {
-                    "css",
-                    "scss",
-                    "less",
-                    "html",
-                    "json",
-                    "jsonc",
-                    "yaml",
-                    "markdown",
-                    "markdown.mdx",
-                    "graphql",
-                    "handlebars",
-                },
-                extra_args = { "--tab-width=" .. vim.o.tabstop, "--config-precedence=prefer-file" },
-            },
-        }
-    end,
+    -- Check supported formatters and linters
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      null_ls.builtins.formatting.shfmt.with { extra_args = { "-i", vim.o.tabstop, "-ci" } },
+      null_ls.builtins.formatting.prettierd.with {
+        filetypes = {
+          "css",
+          "scss",
+          "less",
+          "html",
+          "json",
+          "jsonc",
+          "yaml",
+          "markdown",
+          "markdown.mdx",
+          "graphql",
+          "handlebars",
+        },
+        extra_args = { "--tab-width=" .. vim.o.tabstop, "--config-precedence=prefer-file" },
+      },
+    }
+  end,
 }
