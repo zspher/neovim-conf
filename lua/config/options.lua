f48e3a6e723e3e3442b75482a592c99293b6cc2c
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-11-25 00:03:02 +0800

    feat: add `AstroNvim` setup

---
+++ lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/options.lua
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.signcolumn = "auto"
+
+vim.opt.wrap = false
+-- vim.opt.showbreak = "↪ "
+
+vim.opt.tabstop = 4 -- num of spaces displayed for <tab>
+vim.opt.shiftwidth = 4 -- for << indent
+vim.opt.expandtab = true -- tabs to spaces
+
+vim.opt.conceallevel = 2
+
+vim.opt.list = true
+vim.opt.listchars:append { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" }
+
+vim.opt.spell = true
+
+vim.g.mapleader = " "
