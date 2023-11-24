fb52a2ef4f71f04f105152c4f3333a93f7d66cd4
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-11-25 00:07:05 +0800

    feat: add catppuccin colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "AstroNvim/astroui",
+        ---@type AstroUIOpts
+        opts = {
+            colorscheme = "catppuccin",
+        },
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha",
+            transparent_background = true,
+            integrations = {
+                nvimtree = false,
+
+                aerial = true,
+                leap = true,
+                lsp_trouble = true,
+                mason = true,
+                neotest = true,
+                neotree = true,
+                noice = true,
+                notify = true,
+                overseer = true,
+                symbols_outline = true,
+                telescope = { enabled = true, style = "nvchad" },
+                which_key = true,
+            },
+        },
+    },
+}
