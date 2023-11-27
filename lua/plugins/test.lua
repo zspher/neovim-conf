aaf609b5203adfc2e12b462d190a2764ee335c0f
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-11-28 04:31:46 +0800

    feat: add neotest

---
+++ lua/plugins/test.lua
@@ -0,0 +1,26 @@
+return {
+  { import = "astrocommunity.test.neotest" },
+  {
+    "nvim-neotest/neotest",
+    keys = function()
+      local prefix = "<leader>r"
+      return {
+        { prefix .. "b", function() require("neotest").run.run(vim.fn.expand "%") end, desc = "Test: Run File" },
+        { prefix .. "T", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Test: Run All Test Files" },
+        { prefix .. "S", function() require("neotest").summary.toggle() end, desc = "Test: Toggle Summary" },
+        { prefix .. "O", function() require("neotest").output_panel.toggle() end, desc = "Test: Toggle Output Panel" },
+        { prefix .. "e", function() require("neotest").run.stop() end, desc = "Test: Stop" },
+      }
+    end,
+    opts = function()
+      return {
+        adapters = {
+          require "neotest-python" {
+            dap = { justMyCode = false },
+            pytest_discover_instances = true,
+          },
+        },
+      }
+    end,
+  },
+}
