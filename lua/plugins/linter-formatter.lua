2a39282b0be378b8298094563732f9886cdd690a
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-11 18:12:23 +0800

    refactor: update linter

--- lua/plugins/linter-formatter.lua
+++ lua/plugins/linter-formatter.lua
@@ -1,16 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  opts = function(_, config)
-    local null_ls = require "null-ls"
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require "null-ls"
 
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      null_ls.builtins.formatting.shfmt.with { extra_args = { "-i", vim.o.tabstop, "-ci" } },
-      null_ls.builtins.formatting.prettierd.with {
-        extra_args = { "--tab-width=" .. vim.o.tabstop, "--config-precedence=prefer-file" },
-      },
-    }
-  end,
+      -- Check supported formatters and linters
+      -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+      -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+      opts.root_dir = opts.root_dir
+        or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.fish_indent,
+        nls.builtins.diagnostics.fish,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.shfmt.with { extra_args = { "-i", vim.o.tabstop, "-ci" } },
+        nls.builtins.formatting.prettierd.with {
+          extra_args = { "--tab-width=" .. vim.o.tabstop, "--config-precedence=prefer-file" },
+        },
+      })
+    end,
+  },
 }
