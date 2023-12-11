1225e9d631520eb728919438fd862658750dae02
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-11 16:13:49 +0800

    refactor: update UI

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -1,17 +1,17 @@
 return {
-  { import = "astrocommunity.editing-support/rainbow-delimiters-nvim" },
+  { "echasnovski/mini.indentscope", enabled = false },
   {
-    "nvim-neo-tree/neo-tree.nvim",
+    "lukas-reineke/indent-blankline.nvim",
+    branch = "current-indent",
     opts = {
+      current_indent = { enabled = true, show_start = false, show_end = false, highlight = "RainbowDelimiterBlue" },
     },
-  },
-  {
-    "onsails/lspkind.nvim",
-    opts = {
-      preset = "codicons",
-      symbol_map = { Codeium = "" },
-    },
-  },
+    dependencies = {
+      "HiPhish/rainbow-delimiters.nvim",
+      lazy = true,
+      dependencies = "nvim-treesitter/nvim-treesitter",
+      event = "LazyFile",
+      main = "rainbow-delimiters.setup",
     },
   },
 }
