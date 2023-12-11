870cc5c9ee902830baf17d44229e12861823b608
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-11 16:12:22 +0800

    feat: update editor/lua

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -3,15 +3,6 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          never_show = {
-            ".git",
-          },
-        },
-      },
     },
   },
   {
@@ -19,33 +10,6 @@ return {
     opts = {
       preset = "codicons",
       symbol_map = { Codeium = "" },
-    },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        layout_strategy = "flex",
-        layout_config = {
-          preview_cutoff = 0,
-          flex = { flip_columns = 106 },
-          horizontal = { prompt_position = "top", preview_width = 0.55 },
-          vertical = { prompt_position = "bottom", mirror = false },
-        },
-      },
-      pickers = {
-        find_files = {
-          find_command = { "fd", "--type", "f", "--color", "never", "--hidden", "-E", ".git" },
-        },
-        live_grep = {
-          glob_pattern = { "!.git/" },
-          additional_args = { "--hidden" },
-        },
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
     },
   },
     },
