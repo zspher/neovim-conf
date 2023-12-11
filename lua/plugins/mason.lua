4672926dbb15298a3da23dbf48b3bf3f962a7024
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-11 18:12:07 +0800

    refactor: update mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,19 +4,28 @@ local function diableNixos()
 end
 
 return {
+
+  {
+    "williamboman/mason.nvim",
+    enabled = diableNixos(),
+    optional = true,
+  },
   {
     "williamboman/mason-lspconfig.nvim",
     enabled = diableNixos(),
+    optional = true,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     enabled = diableNixos(),
+    optional = true,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     enabled = diableNixos(),
+    optional = true,
   },
 }
