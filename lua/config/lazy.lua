758e8347217e21e4da08c35184d58c2e14a0eb32
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-11 10:11:09 +0800

    feat: change distro to LazyVim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,35 +1,27 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.g.astronvim_first_install = true -- lets AstroNvim know that this is an initial installation
-    -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-      lazypath })
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
--- TODO: set to true on release
--- Whether or not to use stable releases of AstroNvim
-local USE_STABLE = false
-
 require("lazy").setup {
   spec = {
-    -- TODO: remove branch v4 on release
-    {
-      "AstroNvim/AstroNvim",
-      branch = "v4",
-      version = USE_STABLE and "^4" or nil,
-      import = "astronvim.plugins",
-    },
-    -- pin plugins to known working versions
-    { import = "astronvim.lazy_snapshot", cond = USE_STABLE },
-    -- AstroCommunity: import any community modules here
-    -- TODO: Remove branch v4 on release
-    { "AstroNvim/astrocommunity", branch = "v4" },
-    -- { import = "astrocommunity.pack.lua" },
-    -- import/override with your plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
   },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
   install = { colorscheme = { "catppuccin" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins, add more to your liking
