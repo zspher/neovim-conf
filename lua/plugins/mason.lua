be43a53c07afbb43145043694b706f85be783201
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-11 23:13:17 +0800

    feat: mason favor system packages first

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,31 +1,24 @@
-local function diableNixos()
-  if vim.fn.isdirectory "/nix/var/nix/profiles/system" == 1 then return false end
-  return true
-end
-
 return {
-
   {
     "williamboman/mason.nvim",
-    enabled = diableNixos(),
+    opts = {
+      PATH = "append",
+    },
     optional = true,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    enabled = diableNixos(),
     optional = true,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    enabled = diableNixos(),
     optional = true,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
-    enabled = diableNixos(),
     optional = true,
   },
 }
