a05d1b1fe03d6639300a2a6fc6bf6af67a341861
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-12 19:15:16 +0800

    feat: use conform & nvim-lint for formatter & linter

--- lua/plugins/linter-formatter.lua
+++ lua/plugins/linter-formatter.lua
@@ -1,7 +1,46 @@
 return {
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
   {
+    "mfussenegger/nvim-lint",
+    ops = {
+      linters_by_ft = {
+        fish = { "fish" },
+        bash = { "shellcheck" },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        vue = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        less = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        jsonc = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        ["markdown.mdx"] = { "prettierd" },
+        graphql = { "prettierd" },
+        handlebars = { "prettierd" },
+        bash = { "shfmt" },
+      },
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", vim.o.tabstop, "-ci" },
+        },
+      },
+    },
+  },
+  {
     "nvimtools/none-ls.nvim",
+
     opts = function(_, opts)
       local nls = require "null-ls"
 
