453d8f9e8530277a275e4a641a04c6c2e44796e1
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-12 19:15:16 +0800

    feat: use conform & nvim-lint for formatter & linter

--- lua/plugins/lang/bash.lua
+++ lua/plugins/lang/bash.lua
@@ -15,18 +15,6 @@ return {
     },
   },
   {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require "null-ls"
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.code_actions.shellcheck,
-        nls.builtins.diagnostics.shellcheck,
-        nls.builtins.formatting.shfmt.with { extra_args = { "-i", vim.o.tabstop, "-ci" } },
-      })
-    end,
-  },
-  {
     "jay-babu/mason-nvim-dap.nvim",
     optional = true,
     opts = function(_, opts)
