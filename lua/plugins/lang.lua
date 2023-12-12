d105f2b5a3750654e3fafc091c3ea1ca1450db4d
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-12 14:41:51 +0800

    fix: custom dap config overwritten by lang.clangd

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -11,4 +11,27 @@ return {
       if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "comment" }) end
     end,
   },
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require "dap"
+      for _, lang in ipairs { "c", "cpp" } do
+        dap.configurations[lang] = {
+          {
+            name = "LLDB: Launch",
+            type = "codelldb",
+            request = "launch",
+            program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+            args = {},
+            env = {
+              UBSAN_OPTIONS = "print_stacktrace=1",
+              ASAN_OPTIONS = "abort_on_error=1:fast_unwind_on_malloc=0:detect_leaks=0",
+            },
+          },
+        }
+      end
+    end,
+  },
 }
