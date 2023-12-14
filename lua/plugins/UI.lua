322c18652f9b92557900851e082d076ae97e17f3
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-14 21:16:41 +0800

    feat(lualine): add winbar
    - overwrite aerial plugin defaults

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -1,4 +1,53 @@
+local Util = require "lazyvim.util"
+local icons = require("lazyvim.config").icons
 return {
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  { -- overrides parts of aerial config
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = { section_separators = "", component_separators = { left = "›", right = "|" } },
+      sections = {
+        lualine_c = {
+          Util.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+        },
+      },
+      winbar = {
+        lualine_c = {
+          { Util.lualine.pretty_path() },
+          {
+            "aerial",
+            sep = " › ", -- separator between symbols
+            sep_icon = "", -- separator between icon and symbol
+
+            -- The number of symbols to render top-down. In order to render only 'N' last
+            -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+            -- be used in order to render only current symbol.
+            depth = 5,
+
+            -- When 'dense' mode is on, icons are not rendered near their symbols. Only
+            -- a single icon that represents the kind of current symbol is rendered at
+            -- the beginning of status line.
+            dense = false,
+
+            -- The separator to be used to separate symbols in dense mode.
+            dense_sep = ".",
+
+            -- Color the symbol icons.
+            colored = true,
+          },
+        },
+      },
+    },
+  },
   { "echasnovski/mini.indentscope", enabled = false },
   {
     "lukas-reineke/indent-blankline.nvim",
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.editor.aerial" },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
