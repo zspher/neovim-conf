eb4ed4fa3c32e0ea1f9fd8870d68143472920394
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-15 23:59:10 +0800

    feat: reword some keymap desc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.del("n", "<S-l>")
 vim.keymap.del("n", "s")
 vim.keymap.del("n", "<S-s>")
 
+vim.keymap.set("n", "<S-h>", "<S-h>", { desc = "Top line of window" })
+vim.keymap.set("n", "<S-l>", "<S-l>", { desc = "Bottom line of window" })
+vim.keymap.set("n", "s", "s", { desc = "" })
+vim.keymap.set("n", "<S-s>", "<S-s>", { desc = "" })
