3513d6737ac54601f5b90e169702f2f4c0a43927
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-16 00:00:22 +0800

    feat(lualine): add absolute path to winbar

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -1,11 +1,22 @@
 local Util = require "lazyvim.util"
 local icons = require("lazyvim.config").icons
+local function disable_cond()
+  return function()
+    if vim.bo.bt == "nofile" then return false end
+    return true
+  end
+end
+
 return {
   { import = "lazyvim.plugins.extras.editor.aerial" },
   { -- overrides parts of aerial config
     "nvim-lualine/lualine.nvim",
     opts = {
-      options = { section_separators = "", component_separators = { left = "›", right = "|" } },
+      options = {
+        section_separators = "",
+        component_separators = { left = "›", right = "|" },
+        disabled_filetypes = { winbar = { "aerial", "neo-tree", "dapui", "dap-repl", "qf", "help" } },
+      },
       sections = {
         lualine_c = {
           Util.lualine.root_dir(),
@@ -22,7 +33,26 @@ return {
       },
       winbar = {
         lualine_c = {
-          { Util.lualine.pretty_path() },
+          {
+            "filename",
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
+            path = 3,
+            -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            symbols = {
+              modified = "", -- Text to show when the file is modified.
+              readonly = "", -- Text to show when the file is non-modifiable or readonly.
+              unnamed = "", -- Text to show for unnamed buffers.
+              newfile = "", -- Text to show for newly created file before first write
+            },
+            cond = disable_cond(),
+          },
           {
             "aerial",
             sep = " › ", -- separator between symbols
