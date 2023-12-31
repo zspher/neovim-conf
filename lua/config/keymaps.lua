ed699ff8c6a3c6cd6b9f827e5d9f7a5ab00fe2fc
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-12-31 17:58:01 +0800

    feat(overseer): link makeprg with Overseer
    Signed-off-by: zspher <66728045+zspher@users.noreply.github.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,21 @@ vim.keymap.set("n", "<S-h>", "<S-h>", { desc = "Top line of window" })
 vim.keymap.set("n", "<S-l>", "<S-l>", { desc = "Bottom line of window" })
 vim.keymap.set("n", "s", "s", { desc = "" })
 vim.keymap.set("n", "<S-s>", "<S-s>", { desc = "" })
+
+vim.api.nvim_create_user_command("Make", function(params)
+  -- Insert args at the '$*' in the makeprg
+  local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
+  if num_subs == 0 then cmd = cmd .. " " .. params.args end
+  local task = require("overseer").new_task {
+    cmd = vim.fn.expandcmd(cmd),
+    components = {
+      { "on_output_quickfix", open = not params.bang, open_height = 8, errorformat = vim.o.efm },
+      "default",
+    },
+  }
+  task:start()
+end, {
+  desc = "Run your makeprg as an Overseer task",
+  nargs = "*",
+  bang = true,
+})
