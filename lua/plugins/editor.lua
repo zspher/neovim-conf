53d9b26cb84ef55bf22f2951139fbd95dba31319
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-04 00:03:26 +0800

    feat(neotree): normal mode on input
    Signed-off-by: zspher <66728045+zspher@users.noreply.github.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-neo-tree/neo-tree.nvim",
         opts = {
+            enable_normal_mode_for_inputs = true,
             window = {
                 mappings = {
                     ["a"] = {
