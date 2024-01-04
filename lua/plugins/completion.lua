546aa3f1e866cd72ac9a8f4171d33a5109198026
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-04 14:57:06 +0800

    feat: add neogen
    Signed-off-by: zspher <66728045+zspher@users.noreply.github.com>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -65,4 +65,47 @@ return {
             })
         end,
     },
+    {
+        "danymat/neogen",
+        opts = {
+            snippet_engine = "luasnip",
+        },
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            {
+                "folke/which-key.nvim",
+                optional = true,
+                opts = {
+                    defaults = {
+                        ["<leader>n"] = {
+                            name = "+Neogen",
+                            mode = "n",
+                        },
+                    },
+                },
+            },
+        },
+        keys = {
+            {
+                "<leader>nf",
+                function() require("neogen").generate { type = "func" } end,
+                desc = "function",
+            },
+            {
+                "<leader>nc",
+                function() require("neogen").generate { type = "class" } end,
+                desc = "class",
+            },
+            {
+                "<leader>nt",
+                function() require("neogen").generate { type = "type" } end,
+                desc = "type",
+            },
+            {
+                "<leader>nF",
+                function() require("neogen").generate { type = "file" } end,
+                desc = "file",
+            },
+        },
+    },
 }
