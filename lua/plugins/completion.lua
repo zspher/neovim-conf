c1abdf515b6d057053883d8ad238b40a02696cc6
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-05 13:36:21 +0800

    feat(luasnip): add bash snippets
    Signed-off-by: zspher <66728045+zspher@users.noreply.github.com>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,7 +1,14 @@
 return {
     -- Use <tab> for completion and snippets (supertab)
     -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-    { "L3MON4D3/LuaSnip", keys = function() return {} end },
+    {
+        "L3MON4D3/LuaSnip",
+        config = function()
+            require("luasnip.loaders.from_vscode").load {
+                paths = "./snippets",
+            }
+        end,
+    },
     -- then: setup supertab in cmp
     {
         "hrsh7th/nvim-cmp",
