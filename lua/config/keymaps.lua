3fab9cffd2490782af5bac970099f4b25a893a17
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-11 00:28:10 +0800

    fix: gx without netrw
    Signed-off-by: zspher <66728045+zspher@users.noreply.github.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,28 @@ vim.keymap.set("n", "s", "s", { desc = "" })
 vim.keymap.set("n", "<S-s>", "<S-s>", { desc = "" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- TODO: Remove when dropping support for <Neovim v0.10
+if not vim.ui.open then
+    vim.keymap.set("n", "gx", function(path)
+        -- TODO: REMOVE WHEN DROPPING NEOVIM <0.10
+        if vim.ui.open then return vim.ui.open(path) end
+        local cmd
+        if vim.fn.has "win32" == 1 and vim.fn.executable "explorer" == 1 then
+            cmd = { "cmd.exe", "/K", "explorer" }
+        elseif vim.fn.has "unix" == 1 and vim.fn.executable "xdg-open" == 1 then
+            cmd = { "xdg-open" }
+        elseif
+            (vim.fn.has "mac" == 1 or vim.fn.has "unix" == 1)
+            and vim.fn.executable "open" == 1
+        then
+            cmd = { "open" }
+        end
+        vim.fn.jobstart(
+            vim.fn.extend(cmd, { path or vim.fn.expand "<cfile>" }),
+            { detach = true }
+        )
+    end, { desc = "Open the file under cursor with system app" })
+end
 
 vim.api.nvim_create_user_command("Make", function(params)
     -- Insert args at the '$*' in the makeprg
