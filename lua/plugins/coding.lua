fa761674004323bb05189ebadbaa1c1d78a5e70e
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-11 00:27:31 +0800

    feat: use nvim-autopairs
    Signed-off-by: zspher <66728045+zspher@users.noreply.github.com>

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -143,4 +143,26 @@ return {
             }
         end,
     },
+    { "echasnovski/mini.pairs", enabled = false },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+        keys = {
+            {
+                "<leader>up",
+                function()
+                    local Util = require "lazy.core.util"
+                    require("nvim-autopairs").state.disabled =
+                        not require("nvim-autopairs").state.disabled
+                    if require("nvim-autopairs").state.disabled then
+                        Util.warn("Disabled auto pairs", { title = "Option" })
+                    else
+                        Util.info("Enabled auto pairs", { title = "Option" })
+                    end
+                end,
+                desc = "Toggle auto pairs",
+            },
+        },
+    },
 }
