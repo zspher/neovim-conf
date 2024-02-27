432f3b5ddcf4909a971b384d9daa179f6bab4775
zspher <66728045+zspher@users.noreply.github.com>
2024-02-27 15:14:01 +0800

    feat(nix): add treesitter

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -1,6 +1,14 @@
 ---@type LazySpec[]
 return {
     {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            if type(opts.ensure_installed) == "table" then
+                vim.list_extend(opts.ensure_installed, { "nix" })
+            end
+        end,
+    },
+    {
         "neovim/nvim-lspconfig",
         ---@class PluginLspOpts
         opts = {
