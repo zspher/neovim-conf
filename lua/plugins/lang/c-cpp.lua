1e6896f71551e471729cf3cf1607ff78877732f9
zspher <66728045+zspher@users.noreply.github.com>
2024-02-27 15:13:35 +0800

    feat: use codelldb instead of lldb

--- lua/plugins/lang/c-cpp.lua
+++ lua/plugins/lang/c-cpp.lua
@@ -11,7 +11,6 @@ return {
     },
     {
         "p00f/clangd_extensions.nvim",
-        lazy = true,
         config = function() end,
         opts = {
             inlay_hints = {
@@ -119,23 +118,20 @@ return {
     {
         "mfussenegger/nvim-dap",
         optional = true,
-        dependencies = {
-            -- Ensure C/C++ debugger is installed
-            "williamboman/mason.nvim",
-            optional = true,
-            opts = function(_, opts)
-                if type(opts.ensure_installed) == "table" then
-                    vim.list_extend(opts.ensure_installed, { "codelldb" })
-                end
-            end,
-        },
         opts = function()
             local dap = require "dap"
             for _, lang in ipairs { "c", "cpp" } do
                 dap.configurations[lang] = {
                     {
+                        type = "codelldb",
+                        request = "attach",
+                        name = "Attach to process",
+                        processId = require("dap.utils").pick_process,
+                        cwd = "${workspaceFolder}",
+                    },
+                    {
                         name = "Launch file",
-                        type = "lldb",
+                        type = "codelldb",
                         request = "launch",
                         program = function()
                             return vim.fn.input {
@@ -147,6 +143,10 @@ return {
                         cwd = "${workspaceFolder}",
                         stopOnEntry = false,
                         args = {},
+                        env = {
+                            UBSAN_OPTIONS = "print_stacktrace=1",
+                            ASAN_OPTIONS = "abort_on_error=1:fast_unwind_on_malloc=0:detect_leaks=0",
+                        },
                     },
                 }
             end
