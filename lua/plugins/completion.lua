923af7921df330b868c366205d0bf6487d20a27e
zspher <66728045+zspher@users.noreply.github.com>
2024-05-10 23:23:35 +0800

    feat: remove codeium

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -85,23 +85,6 @@ return {
         end,
     },
     {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-    },
-    {
-        "nvim-cmp",
-        ---@param opts cmp.ConfigSchema
-        opts = function(_, opts)
-            table.insert(opts.sources, 1, {
-                name = "codeium",
-                group_index = 1,
-                priority = 100,
-            })
-        end,
-    },
-    {
         "danymat/neogen",
         opts = {
             snippet_engine = "luasnip",
