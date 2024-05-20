dbb7718cb409fa42043a608259763f9c73744769
zspher <66728045+zspher@users.noreply.github.com>
2024-05-20 21:51:39 +0800

    feat: use indent-blankline main

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -112,23 +112,11 @@ return {
         },
     },
     {
-        "lukas-reineke/indent-blankline.nvim",
-        branch = "current-indent",
-        opts = {
-            current_indent = {
-                enabled = true,
-                show_start = false,
-                show_end = false,
-                highlight = "RainbowDelimiterBlue",
-            },
-        },
-        dependencies = {
-            "HiPhish/rainbow-delimiters.nvim",
-            lazy = true,
-            dependencies = "nvim-treesitter/nvim-treesitter",
-            event = "LazyFile",
-            main = "rainbow-delimiters.setup",
-        },
+        "HiPhish/rainbow-delimiters.nvim",
+        lazy = true,
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        event = "LazyFile",
+        main = "rainbow-delimiters.setup",
     },
     {
         "nvchad/nvim-colorizer.lua",
