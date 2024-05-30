fa1a4166df475c47627879ce4eeb1d96c4a4d792
zspher <66728045+zspher@users.noreply.github.com>
2024-05-30 17:36:04 +0800

    feat(lang.rust): change keymaps
    - code actions should be loaded
    - on nvim > 0.9: debuggables should load in nvim-dap

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -9,18 +9,6 @@ return {
                 on_attach = function(_, bufnr)
                     vim.keymap.set(
                         "n",
-                        "<leader>cR",
-                        function() vim.cmd.RustLsp "codeAction" end,
-                        { desc = "Code Action", buffer = bufnr }
-                    )
-                    vim.keymap.set(
-                        "n",
-                        "<leader>dr",
-                        function() vim.cmd.RustLsp "debuggables" end,
-                        { desc = "Rust Debuggables", buffer = bufnr }
-                    )
-                    vim.keymap.set(
-                        "n",
                         "<leader>rt",
                         function() vim.cmd.RustLsp "runnables" end,
                         { desc = "Rust run task", buffer = bufnr }
