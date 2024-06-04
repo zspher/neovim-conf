51d903fcfc752b5eafdd57115bff5ac1bb77e195
zspher <66728045+zspher@users.noreply.github.com>
2024-06-04 23:51:22 +0800

    feat(lang.cs): remove treesitter c# as this messes with LSP highlighting

--- lua/plugins/lang/cs.lua
+++ lua/plugins/lang/cs.lua
@@ -4,7 +4,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         opts = function(_, opts)
             if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "c_sharp", "xml" })
+                vim.list_extend(opts.ensure_installed, { "xml" })
             end
         end,
     },
