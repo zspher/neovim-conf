653196aedb91c76543d2b610fc2f9e1e5ba95339
zspher <66728045+zspher@users.noreply.github.com>
2024-06-08 22:10:10 +0800

    refactor: simplify tresitter ensure_installed

--- lua/plugins/lang/bash.lua
+++ lua/plugins/lang/bash.lua
@@ -2,11 +2,9 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "bash" })
-            end
-        end,
+        opts = {
+            ensure_installed = { "bash" },
+        },
     },
     {
         "neovim/nvim-lspconfig",
