e226dabafe2cc43a621656e5fcf41898c0309ca8
zspher <66728045+zspher@users.noreply.github.com>
2024-06-12 13:05:45 +0800

    feat(lang.cs): add more handlers

--- lua/plugins/lang/cs.lua
+++ lua/plugins/lang/cs.lua
@@ -19,6 +19,28 @@ return {
             servers = {
                 omnisharp = {
                     cmd = { "OmniSharp" },
+                    handlers = {
+                        ["textDocument/definition"] = function(...)
+                            return require("omnisharp_extended").definition_handler(
+                                ...
+                            )
+                        end,
+                        ["textDocument/typeDefinition"] = function(...)
+                            return require("omnisharp_extended").type_definition_handler(
+                                ...
+                            )
+                        end,
+                        ["textDocument/references"] = function(...)
+                            return require("omnisharp_extended").references_handler(
+                                ...
+                            )
+                        end,
+                        ["textDocument/implementation"] = function(...)
+                            return require("omnisharp_extended").implementation_handler(
+                                ...
+                            )
+                        end,
+                    },
                     keys = {
                         {
                             "gd",
