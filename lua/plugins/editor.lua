c3bdc60d5f2f8e52d53f6b5633e98ba7c0a71f9a
zspher <66728045+zspher@users.noreply.github.com>
2024-06-13 15:12:42 +0800

    feat(neo-tree): use neo_tree_popup_input_ready as enable_normal_mode_for_inputs is depreciated

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,7 +3,21 @@ return {
     {
         "nvim-neo-tree/neo-tree.nvim",
         opts = {
-            enable_normal_mode_for_inputs = true,
+            event_handlers = {
+                {
+                    event = "neo_tree_popup_input_ready",
+                    ---@param args { bufnr: integer, winid: integer }
+                    handler = function(args)
+                        vim.cmd "stopinsert"
+                        vim.keymap.set(
+                            "i",
+                            "<esc>",
+                            vim.cmd.stopinsert,
+                            { noremap = true, buffer = args.bufnr }
+                        )
+                    end,
+                },
+            },
             window = {
                 mappings = {
                     ["a"] = {
