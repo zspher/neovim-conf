2c803f48ed9b610fd092e472bd7854417eed76a3
zspher <66728045+zspher@users.noreply.github.com>
2024-06-26 20:57:19 +0800

    refactor: remove redundant keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,5 @@
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
-vim.keymap.del("n", "s")
-vim.keymap.del("n", "<S-s>")
 
 vim.keymap.set("n", "<S-h>", "<S-h>", { desc = "Top line of window" })
 vim.keymap.set("n", "<S-l>", "<S-l>", { desc = "Bottom line of window" })
