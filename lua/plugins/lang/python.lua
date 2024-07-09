bc12933fbf40c758b5080fe9af0078270d69b146
zspher <66728045+zspher@users.noreply.github.com>
2024-07-10 02:22:03 +0800

    feat(lang.python): lsp pyright scrict mode

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -18,4 +18,18 @@ return {
         optional = true,
         config = function() require("dap-python").setup "python" end,
     },
+    {
+
+        "neovim/nvim-lspconfig",
+        ---@class PluginLspOpts
+        opts = {
+            servers = {
+                pyright = {
+                    settings = {
+                        python = { analysis = { typeCheckingMode = "strict" } },
+                    },
+                },
+            },
+        },
+    },
 }
