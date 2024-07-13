208336d81583147d8fbe378b7dce3c71591f3be2
zspher <66728045+zspher@users.noreply.github.com>
2024-07-13 17:08:05 +0800

    feat: 2 space indent in css, json by default

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("FileType", {
     desc = "set tabstop 2",
-    pattern = { "markdown", "yaml" },
+    pattern = { "markdown", "yaml", "css", "json", "jsonc" },
     callback = function()
         vim.o.tabstop = 2
         vim.o.shiftwidth = 2
