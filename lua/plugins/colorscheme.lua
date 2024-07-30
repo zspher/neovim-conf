c8ead339bbcdef44ca6f35d2a29dd983290be835
zspher <66728045+zspher@users.noreply.github.com>
2024-07-31 01:48:46 +0800

    feat(catppuccin): add initial color highlights for fzf-lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -28,11 +28,26 @@ return {
                 notify = true,
                 which_key = true,
             },
-            custom_highlights = function(colors)
+            custom_highlights = function(c)
                 return {
-                    LineNrAbove = { fg = colors.subtext0 },
-                    LineNrBelow = { fg = colors.subtext0 },
-                    CursorLineNr = { fg = colors.peach, style = { "bold" } },
+                    LineNrAbove = { fg = c.subtext0 },
+                    LineNrBelow = { fg = c.subtext0 },
+                    CursorLineNr = { fg = c.peach, style = { "bold" } },
+
+                    -- fzf colos
+                    FzfLuaBorder = { link = "FloatBorder" },
+                    FzfLuaTitle = { link = "FloatBorder" },
+                    FzfLuaHeaderBind = { fg = c.yellow },
+                    FzfLuaBufNr = { fg = c.yellow },
+                    FzfLuaTabMarker = { fg = c.yellow },
+                    FzfLuaHeaderText = { fg = c.peach },
+                    FzfLuaBufFlagCur = { fg = c.peach },
+                    FzfLuaLiveSym = { fg = c.peach },
+                    FzfLuaPathColNr = { fg = c.blue },
+                    FzfLuaBufFlagAlt = { fg = c.blue },
+                    FzfLuaTabTitle = { fg = c.sky },
+                    FzfLuaPathLineNr = { fg = c.green },
+                    FzfLuaBufName = { fg = c.mauve },
                 }
             end,
         },
