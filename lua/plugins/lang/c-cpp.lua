a276b636e1c4425446d1b5d09bf51f9890d4c7e0
zspher <66728045+zspher@users.noreply.github.com>
2024-09-17 13:24:34 +0800

    refactor(lang): c-cpp treesitter use lazy.nvim `opt_extend`

--- lua/plugins/lang/c-cpp.lua
+++ lua/plugins/lang/c-cpp.lua
@@ -21,12 +21,9 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "doxygen" })
-                --
-            end
-        end,
+        opts = {
+            ensure_installed = { "doxygen" },
+        },
     },
     {
         "mfussenegger/nvim-dap",
