539c301f17e9d7fa70789010f35e5cc79a9059f9
zspher <66728045+zspher@users.noreply.github.com>
2024-10-25 15:01:01 +0800

    feat(lang.nix): add nixd

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -1,4 +1,5 @@
 ---@type LazySpec[]
+local flake = '(builtins.getFlake "' .. vim.env.HOME .. '/dotfiles")'
 return {
     {
         "nvim-treesitter/nvim-treesitter",
@@ -11,6 +12,29 @@ return {
         ---@class PluginLspOpts
         opts = {
             servers = {
+                nixd = {
+                    settings = {
+                        ["nixd"] = {
+                            nixpkgs = { expr = "import <nixpkgs> { }" },
+                            ["options"] = {
+                                ["nixos"] = {
+                                    expr = flake
+                                        .. ".nixosConfigurations.ls-2100.options",
+                                },
+                                ["home-manager"] = {
+                                    expr = flake
+                                        .. '.homeConfigurations."faust@ls-2100".options',
+                                },
+                                ["flake-parts"] = {
+                                    expr = flake .. ".debug.options",
+                                },
+                                ["flake-parts2"] = {
+                                    expr = flake .. ".currentSystem.options",
+                                },
+                            },
+                        },
+                    },
+                },
                 nil_ls = {
                     settings = {
                         ["nil"] = {
