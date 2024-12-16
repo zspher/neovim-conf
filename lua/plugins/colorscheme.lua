85f14f63daa2c3859bc5f1087bd866d8321594d4
zspher <66728045+zspher@users.noreply.github.com>
2024-12-15 20:41:54 -0700

    feat(colorscheme): add snacks.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -30,12 +30,14 @@ return {
                 noice = true,
                 notify = true,
                 which_key = true,
+                snacks = true,
             },
             custom_highlights = function(c)
                 return {
                     LineNrAbove = { fg = c.subtext0 },
                     LineNrBelow = { fg = c.subtext0 },
                     CursorLineNr = { fg = c.peach, style = { "bold" } },
+                    SnacksIndent1 = { fg = c.blue },
                 }
             end,
         },
