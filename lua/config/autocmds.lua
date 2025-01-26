fec6fe80fe0633aa8dae1a46121c0ec75a9f4442
zspher <66728045+zspher@users.noreply.github.com>
2025-01-26 16:43:26 -0700

    feat: html & xml 2 space indent by default

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("FileType", {
     desc = "set tabstop 2",
-    pattern = { "markdown", "yaml", "css", "json", "jsonc" },
+    pattern = { "markdown", "yaml", "css", "json", "jsonc", "html", "xml" },
     callback = function()
         vim.o.tabstop = 2
         vim.o.shiftwidth = 2
