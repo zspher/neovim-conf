1abe6f5bcdb25c472025ff5920069751fa245e4c
zspher <66728045+zspher@users.noreply.github.com>
2025-01-29 20:22:31 -0700

    feat(lang.sql): conform sqlfluff not require_cwd

--- lua/plugins/lang/sql.lua
+++ lua/plugins/lang/sql.lua
@@ -1,4 +1,13 @@
 ---@type LazySpec[]
 return {
     { import = "lazyvim.plugins.extras.lang.sql" },
+    {
+        "stevearc/conform.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.formatters.sqlfluff = {
+                require_cwd = false,
+            }
+        end,
+    },
 }
