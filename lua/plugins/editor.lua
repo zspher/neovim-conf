fd20ec1bcfe2bfb4d81fb785346fc1e013aefb71
zspher <66728045+zspher@users.noreply.github.com>
2025-02-11 14:08:24 -0700

    feat(oil): add snacks.rename

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -126,6 +126,21 @@ return {
                 signcolumn = "yes",
             },
         },
+        config = function(_, opts)
+            require("oil").setup(opts)
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "OilActionsPost",
+                callback = function(event)
+                    if event.data.actions.type == "move" then
+                        require("snacks").rename.on_rename_file(
+                            event.data.actions.src_url,
+                            event.data.actions.dest_url
+                        )
+                    end
+                end,
+            })
+        end,
         keys = {
             {
                 "<leader>e",
