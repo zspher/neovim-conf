fa06176367f94bfb384c68497634e7611d09d5b2
zspher <66728045+zspher@users.noreply.github.com>
2025-06-05 14:06:52 -0600

    fix: no filetype on first open

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,10 @@
 return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
+    -- FIX: https://github.com/LazyVim/LazyVim/issues/6151
+    event = vim.fn.has "nvim-0.11" == 1
+            and { "BufReadPre", "BufNewFile", "BufWritePre" }
+        or { "LazyFile" },
     opts = function(opts, _)
         opts.codelens = {
             enabled = true,
