edbfc042a118d7686c712256e47de6b0a7396412
zspher <66728045+zspher@users.noreply.github.com>
2025-08-19 16:10:43 -0600

    feat: add some opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,17 +3,21 @@
 
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.opt.signcolumn = "auto"
-
+vim.opt.mouse = "a"
+vim.opt.signcolumn = "yes"
+vim.opt.cc = "80"
+vim.opt.showmode = false -- Dont show mode since we have a statusline
 vim.opt.wrap = false
--- vim.opt.showbreak = "↪ "
 
+vim.schedule(
+    function() vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" end
+)
+
+vim.opt.showbreak = "↪ "
 vim.opt.tabstop = 4 -- num of spaces displayed for <tab>
 vim.opt.shiftwidth = 4 -- for << indent
 vim.opt.expandtab = true -- tabs to spaces
-
 vim.opt.conceallevel = 2
-
 vim.opt.list = true
 vim.opt.listchars = {
     tab = "│·→",
@@ -24,11 +28,25 @@ vim.opt.listchars = {
     nbsp = "␣",
 }
 
-vim.opt.cc = "80"
-
 vim.opt.spell = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
 
-vim.g.mapleader = " "
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.smoothscroll = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 vim.filetype.add {
     extension = {
@@ -43,6 +61,7 @@ vim.filetype.add {
     },
 }
 
+vim.g.mapleader = " "
 vim.g.snacks_animate = false
 vim.g.lazyvim_check_order = false
 vim.g.lazyvim_picker = "snacks"
