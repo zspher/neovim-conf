276f9d4a625ad3a8d7ce65dc454a3a687c89b146
zspher <66728045+zspher@users.noreply.github.com>
2025-08-22 14:11:20 -0600

    feat: add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,60 @@
+return {
+    {
+        "folke/which-key.nvim",
+        opts = {
+            spec = {
+                { "<BS>", desc = "Decrement Selection", mode = "x" },
+                {
+                    "<c-space>",
+                    desc = "Increment Selection",
+                    mode = { "x", "n" },
+                },
+            },
+        },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false, -- last release is way too old and doesn't work on Windows
+        build = ":TSUpdate",
+        event = { "VeryLazy" },
+        main = "nvim-treesitter.configs",
+        lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+        init = function(plugin)
+            -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+            -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+            -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+            -- Luckily, the only things that those plugins need are the custom queries, which we make available
+            -- during startup.
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require "nvim-treesitter.query_predicates"
+        end,
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        keys = {
+            { "<c-space>", desc = "Increment Selection" },
+            { "<bs>", desc = "Decrement Selection", mode = "x" },
+        },
+        opts_extend = { "ensure_installed" },
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
+        opts = {
+            ensure_installed = {
+                "diff",
+                "query",
+                "vim",
+                "vimdoc",
+            },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        },
+    },
+}
