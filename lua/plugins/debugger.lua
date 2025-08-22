0f795e5889f9a7964b7ad8d65f08cc95c43d36e5
zspher <66728045+zspher@users.noreply.github.com>
2025-08-22 17:35:39 -0600

    feat(debugger): switch to nvim-dap-view and use persistent breakpoints

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -27,7 +27,7 @@ return {
         desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
 
         dependencies = {
-            "rcarriga/nvim-dap-ui",
+            "igorlfs/nvim-dap-view",
             -- virtual text for the debugger
             {
                 "theHamsta/nvim-dap-virtual-text",
@@ -37,6 +37,91 @@ return {
 
         keys = function(_, keys)
             local dap = require "dap"
+            local default_keys = {
+                {
+                    "<leader>db",
+                    dap.toggle_breakpoint,
+                    desc = "Toggle Breakpoint",
+                },
+                { "<leader>dc", dap.continue, desc = "Run/Continue" },
+                {
+                    "<leader>da",
+                    function() dap.continue { before = get_args } end,
+                    desc = "Run with Args",
+                },
+                { "<leader>dC", dap.run_to_cursor, desc = "Run to Cursor" },
+                { "<leader>dg", dap.goto_, desc = "Go to Line (No Execute)" },
+                { "<leader>di", dap.step_into, desc = "Step Into" },
+                { "<leader>dj", dap.down, desc = "Down" },
+                { "<leader>dk", dap.up, desc = "Up" },
+                { "<leader>dl", dap.run_last, desc = "Run Last" },
+                { "<leader>do", dap.step_out, desc = "Step Out" },
+                { "<leader>dO", dap.step_over, desc = "Step Over" },
+                { "<leader>dP", dap.pause, desc = "Pause" },
+                { "<leader>dr", dap.repl.toggle, desc = "Toggle REPL" },
+                { "<leader>ds", dap.session, desc = "Session" },
+                { "<leader>dt", dap.terminate, desc = "Terminate" },
+                {
+                    "<leader>dw",
+                    require("dap.ui.widgets").hover,
+                    desc = "Widgets",
+                },
+                { "<F5>", dap.continue, desc = "Debugger: Start" },
+                { "<F17>", dap.terminate, desc = "Debugger: Stop" }, -- Shift+F5
+                { "<F29>", dap.restart_frame, desc = "Debugger: Restart" }, -- Control+F5
+                { "<F6>", dap.pause, desc = "Debugger: Pause" },
+                { "<F7>", dap.reverse_continue, "Debugger: Reverse" },
+                { "<F10>", dap.step_over, desc = "Debugger: Step Over" },
+                { "<F11>", dap.step_into, desc = "Debugger: Step Into" },
+                { "<F23>", dap.step_out, desc = "Debugger: Step Out" }, -- Shift+F11
+            }
+            return vim.tbl_deep_extend("force", default_keys, keys)
+        end,
+
+        config = function()
+            -- add jsonc support to launch.json support
+            local vscode = require "dap.ext.vscode"
+            local json = require "plenary.json"
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vscode.json_decode = function(str)
+                return vim.json.decode(json.json_strip_comments(str))
+            end
+
+            local dap = require "dap"
+            dap.adapters["netcoredbg"] = {
+                type = "executable",
+                command = "netcoredbg",
+                args = { "--interpreter=vscode" },
+            }
+            dap.adapters["codelldb"] = {
+                type = "executable",
+                command = "codelldb",
+            }
+            dap.adapters["pwa-node"] = {
+                type = "server",
+                host = "localhost",
+                port = "${port}",
+                executable = {
+                    command = "js-debug",
+                    args = {
+                        "${port}",
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "Weissle/persistent-breakpoints.nvim",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        event = { "BufReadPost" },
+        opts = {
+            load_breakpoints_event = { "BufReadPost" },
+        },
+        keys = function(_, keys)
+            local dap = require "dap"
+            local breakpoint = require "persistent-breakpoints.api"
 
             local function edit_breakpoint()
                 -- Search for an existing breakpoint on this line in this buffer
@@ -107,94 +192,50 @@ return {
                 end
 
                 customize_bp(find_bp())
+                breakpoint.breakpoints_changed_in_current_buffer()
             end
-            -- stylua: ignore
             local default_keys = {
-                { "<leader>dB", edit_breakpoint, desc = "Breakpoint Condition" },
-                { "<leader>db", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
-                { "<leader>dc", dap.continue, desc = "Run/Continue" },
-                { "<leader>da", function() dap.continue({ before = get_args }) end, desc = "Run with Args" },
-                { "<leader>dC", dap.run_to_cursor, desc = "Run to Cursor" },
-                { "<leader>dg", dap.goto_, desc = "Go to Line (No Execute)" },
-                { "<leader>di", dap.step_into, desc = "Step Into" },
-                { "<leader>dj", dap.down, desc = "Down" },
-                { "<leader>dk", dap.up, desc = "Up" },
-                { "<leader>dl", dap.run_last, desc = "Run Last" },
-                { "<leader>do", dap.step_out, desc = "Step Out" },
-                { "<leader>dO", dap.step_over, desc = "Step Over" },
-                { "<leader>dP", dap.pause, desc = "Pause" },
-                { "<leader>dr", dap.repl.toggle, desc = "Toggle REPL" },
-                { "<leader>ds", dap.session, desc = "Session" },
-                { "<leader>dt", dap.terminate, desc = "Terminate" },
-                { "<leader>dw", require("dap.ui.widgets").hover, desc = "Widgets" },
-                { "<F5>", dap.continue, desc = "Debugger: Start" },
-                { "<F17>", dap.terminate, desc = "Debugger: Stop", }, -- Shift+F5
-                { "<F29>", dap.restart_frame, desc = "Debugger: Restart", }, -- Control+F5
-                { "<F6>", dap.pause, desc = "Debugger: Pause", },
-                { "<F7>", dap.reverse_continue, "Debugger: Reverse"},
-                { "<F9>", dap.toggle_breakpoint, desc = "Debugger: Toggle Breakpoint", },
-                { "<F10>", dap.step_over, desc = "Debugger: Step Over", },
-                { "<F11>", dap.step_into, desc = "Debugger: Step Into", },
-                { "<F23>", dap.step_out, desc = "Debugger: Step Out", }, -- Shift+F11
-            }
-            return vim.tbl_deep_extend("force", default_keys, keys)
-        end,
-
-        config = function()
-            -- add jsonc support to launch.json support
-            local vscode = require "dap.ext.vscode"
-            local json = require "plenary.json"
-            ---@diagnostic disable-next-line: duplicate-set-field
-            vscode.json_decode = function(str)
-                return vim.json.decode(json.json_strip_comments(str))
-            end
-
-            local dap = require "dap"
-            dap.adapters["netcoredbg"] = {
-                type = "executable",
-                command = "netcoredbg",
-                args = { "--interpreter=vscode" },
-            }
-            dap.adapters["codelldb"] = {
-                type = "executable",
-                command = "codelldb",
-            }
-            dap.adapters["pwa-node"] = {
-                type = "server",
-                host = "localhost",
-                port = "${port}",
-                executable = {
-                    command = "js-debug",
-                    args = {
-                        "${port}",
-                    },
+                {
+                    "<leader>dB",
+                    edit_breakpoint,
+                    desc = "Breakpoint Condition",
+                },
+                {
+                    "<F9>",
+                    breakpoint.toggle_breakpoint,
+                    desc = "Debugger: Toggle Breakpoint",
+                },
+                {
+                    "<F33>",
+                    breakpoint.clear_all_breakpoints,
+                    desc = "Debugger: Clear All Breakpoints",
                 },
             }
+            return vim.tbl_deep_extend("force", default_keys, keys)
         end,
     },
 
     -- fancy UI for the debugger
     {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "nvim-neotest/nvim-nio" },
+        "igorlfs/nvim-dap-view",
         -- stylua: ignore
         keys = {
-          { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-          { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "x"} },
+          { "<leader>du", function() require("dap-view").toggle({ }) end, desc = "Dap UI" },
+          { "<leader>de", function() require("dap-view").add_expr() end, desc = "Watch", mode = {"n", "x"} },
         },
         opts = {},
         config = function(_, opts)
             local dap = require "dap"
-            local dapui = require "dapui"
-            dapui.setup(opts)
+            local dapview = require "dap-view"
+            dapview.setup(opts)
             dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open {}
+                dapview.open()
             end
             dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close {}
+                dapview.close()
             end
             dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close {}
+                dapview.close()
             end
         end,
     },
