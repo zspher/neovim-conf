8feeaf61372a72f431c23cc86771f9cf3251e6b6
zspher <66728045+zspher@users.noreply.github.com>
2025-08-22 11:53:30 -0600

    feat: close some filetypes with `q`

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,37 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.o.shiftwidth = 2
     end,
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "PlenaryTestPopup",
+        "checkhealth",
+        "dbout",
+        "gitsigns-blame",
+        "grug-far",
+        "help",
+        "lspinfo",
+        "neotest-output",
+        "neotest-output-panel",
+        "neotest-summary",
+        "notify",
+        "qf",
+        "startuptime",
+        "tsplayground",
+        "dap-float",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.schedule(function()
+            vim.keymap.set("n", "q", function()
+                vim.cmd "close"
+                pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+            end, {
+                buffer = event.buf,
+                silent = true,
+                desc = "Quit buffer",
+            })
+        end)
+    end,
+})
