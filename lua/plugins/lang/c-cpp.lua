74c3eccd69e48b869b84d93ac9ef838d11910e9b
zspher <66728045+zspher@users.noreply.github.com>
2025-08-22 15:42:53 -0600

    feat: move lang/c-cpp

--- lua/plugins/lang/c-cpp.lua
+++ lua/plugins/lang/c-cpp.lua
@@ -1,5 +1,6 @@
 ---@type LazySpec[]
 return {
+    -- lsp
     {
         "neovim/nvim-lspconfig",
         ---@class PluginLspOpts
@@ -28,19 +29,65 @@ return {
             },
         },
     },
+    -- formatter
+
+    -- linter
+
+    -- syntax highlight
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
             ensure_installed = { "doxygen", "cpp" },
         },
     },
+
+    -- test suite
+
+    -- dap
+
     {
         "mfussenegger/nvim-dap",
         optional = true,
         opts = function()
             local dap = require "dap"
+            local launch
+            launch = {
+                name = "Launch and replay file",
+                type = "codelldb",
+                request = "launch",
+                cwd = "${workspaceFolder}",
+                reverseDebugging = true,
+                program = function()
+                    local utils = require "dap.utils"
+                    local file = utils.pick_file() --[[@as string]]
+                    local port = "12345"
+                    vim.system {
+                        "rr",
+                        "record",
+                        file,
+                    }
+                    vim.system { "rr", "replay", "-s", port }
+                    launch.processCreateCommands = {
+                        "gdb-remote 127.0.0.1:" .. port,
+                    }
+                    return file
+                end,
+            }
             for _, lang in ipairs { "c", "cpp" } do
                 dap.configurations[lang] = {
+                    launch,
+                    {
+                        name = "Launch file",
+                        type = "codelldb",
+                        request = "launch",
+                        program = "${command:pickFile}",
+                        cwd = "${workspaceFolder}",
+                        args = {},
+                        env = {
+                            UBSAN_OPTIONS = "print_stacktrace=1",
+                            ASAN_OPTIONS = "abort_on_error=1:fast_unwind_on_malloc=0:detect_leaks=0",
+                        },
+                    },
                     {
                         type = "codelldb",
                         request = "attach",
@@ -48,27 +95,9 @@ return {
                         processId = require("dap.utils").pick_process,
                         cwd = "${workspaceFolder}",
                     },
-                    {
-                        name = "Launch file",
-                        type = "codelldb",
-                        request = "launch",
-                        program = function()
-                            return vim.fn.input {
-                                input = "Path to executable: ",
-                                text = vim.fn.getcwd() .. "/",
-                                completion = "file",
-                            }
-                        end,
-                        cwd = "${workspaceFolder}",
-                        stopOnEntry = false,
-                        args = {},
-                        env = {
-                            UBSAN_OPTIONS = "print_stacktrace=1",
-                            ASAN_OPTIONS = "abort_on_error=1:fast_unwind_on_malloc=0:detect_leaks=0",
-                        },
-                    },
                 }
             end
         end,
     },
+    -- extra
 }
