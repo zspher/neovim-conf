1b5b242fba0b82af49f4f0c80e38d6a7fa70901a
zspher <66728045+zspher@users.noreply.github.com>
2025-08-22 15:44:22 -0600

    feat: add lang/lua

---
+++ lua/plugins/lang/lua.lua
@@ -0,0 +1,63 @@
+---@type LazySpec[]
+return {
+    -- lsp
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            ---@type table<string, vim.lsp.Config>
+            servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            workspace = {
+                                checkThirdParty = false,
+                            },
+                            codeLens = {
+                                enable = true,
+                            },
+                            completion = {
+                                callSnippet = "Replace",
+                            },
+                            doc = {
+                                privateName = { "^_" },
+                            },
+                            hint = {
+                                enable = true,
+                                setType = false,
+                                paramType = true,
+                                paramName = "All",
+                                semicolon = "Disable",
+                                arrayIndex = "Disable",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+    -- formatter
+    {
+        "stevearc/conform.nvim",
+        optional = true,
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+            },
+        },
+    },
+    -- linter
+
+    -- syntax highlight
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "lua", "luadoc" },
+        },
+    },
+
+    -- test suite
+
+    -- dap
+
+    -- extra
+}
