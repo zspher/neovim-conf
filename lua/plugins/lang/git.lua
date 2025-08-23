243393d59c170729c74e9d79549f525e43c4c278
zspher <66728045+zspher@users.noreply.github.com>
2025-08-23 17:44:00 -0600

    feat: move lang/git

---
+++ lua/plugins/lang/git.lua
@@ -0,0 +1,48 @@
+---@type LazySpec[]
+return {
+
+    -- lsp
+
+    -- formatter
+
+    -- linter
+
+    -- syntax highlight
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "git_config",
+                "gitcommit",
+                "git_rebase",
+                "gitignore",
+                "gitattributes",
+            },
+        },
+    },
+
+    -- test suite
+
+    -- dap
+
+    -- extra
+    {
+        "saghen/blink.cmp",
+        optional = true,
+        dependencies = {
+            "Kaiser-Yang/blink-cmp-git",
+        },
+
+        opts = {
+            sources = {
+                default = { "git" },
+                providers = {
+                    git = {
+                        name = "Git",
+                        module = "blink-cmp-git",
+                    },
+                },
+            },
+        },
+    },
+}
