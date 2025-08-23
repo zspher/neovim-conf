47cce7a14c9f463631d6e36c705d08d88739bf61
zspher <66728045+zspher@users.noreply.github.com>
2025-08-23 12:56:59 -0600

    feat: change dap order for better lazy load

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -19,8 +19,104 @@ local function get_args(config)
     return config
 end
 
+local function edit_breakpoint()
+    -- Search for an existing breakpoint on this line in this buffer
+    ---@return dap.SourceBreakpoint bp that was either found, or an empty placeholder
+    local function find_bp()
+        local buf_bps =
+            require("dap.breakpoints").get(vim.fn.bufnr())[vim.fn.bufnr()]
+        ---@type dap.SourceBreakpoint
+        local bp = {
+            condition = "",
+            logMessage = "",
+            hitCondition = "",
+            line = vim.fn.line ".",
+        }
+        for _, candidate in ipairs(buf_bps) do
+            if candidate.line and candidate.line == vim.fn.line "." then
+                bp = candidate
+                break
+            end
+        end
+        return bp
+    end
+
+    -- Elicit customization via a UI prompt
+    ---@param bp dap.SourceBreakpoint a breakpoint
+    local function customize_bp(bp)
+        local props = {
+            ["Condition"] = {
+                value = bp.condition,
+                setter = function(v) bp.condition = v end,
+            },
+            ["Hit Condition"] = {
+                value = bp.hitCondition,
+                setter = function(v) bp.hitCondition = v end,
+            },
+            ["Log Message"] = {
+                value = bp.logMessage,
+                setter = function(v) bp.logMessage = v end,
+            },
+        }
+        local menu_options = {}
+        for k, v in pairs(props) do
+            table.insert(menu_options, ("%s: %s"):format(k, v.value))
+        end
+        vim.ui.select(menu_options, {
+            prompt = "Edit Breakpoint",
+        }, function(choice)
+            local prompt = (tostring(choice)):gsub(":.*", "")
+            props[prompt].setter(vim.fn.input {
+                prompt = prompt,
+                default = props[prompt].value,
+            })
+
+            -- Set breakpoint for current line, with customizations (see h:dap.set_breakpoint())
+            require("dap").set_breakpoint(
+                bp.condition,
+                bp.hitCondition,
+                bp.logMessage
+            )
+        end)
+    end
+
+    customize_bp(find_bp())
+    require("persistent-breakpoints.api").breakpoints_changed_in_current_buffer()
+end
+
 ---@type LazySpec[]
 return {
+    {
+        "Weissle/persistent-breakpoints.nvim",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        opts = {
+            load_breakpoints_event = "BufRead",
+            always_reload = true,
+        },
+        keys = {
+            {
+                "<leader>dB",
+                edit_breakpoint,
+                desc = "Breakpoint Condition",
+            },
+            {
+                "<F9>",
+                function()
+                    require("persistent-breakpoints.api").toggle_breakpoint()
+                end,
+                desc = "Debugger: Toggle Breakpoint",
+            },
+            {
+                "<F33>",
+                function()
+                    require("persistent-breakpoints.api").clear_all_breakpoints()
+                end,
+                desc = "Debugger: Clear All Breakpoints",
+            },
+        },
+    },
     {
         "mfussenegger/nvim-dap",
         dependencies = {
@@ -30,50 +126,127 @@ return {
                 "theHamsta/nvim-dap-virtual-text",
                 opts = {},
             },
+            "Weissle/persistent-breakpoints.nvim",
         },
-
-        keys = function(_, keys)
-            local dap = require "dap"
-            local default_keys = {
-                {
-                    "<leader>db",
-                    dap.toggle_breakpoint,
-                    desc = "Toggle Breakpoint",
-                },
-                { "<leader>dc", dap.continue, desc = "Run/Continue" },
-                {
-                    "<leader>da",
-                    function() dap.continue { before = get_args } end,
-                    desc = "Run with Args",
-                },
-                { "<leader>dC", dap.run_to_cursor, desc = "Run to Cursor" },
-                { "<leader>dg", dap.goto_, desc = "Go to Line (No Execute)" },
-                { "<leader>di", dap.step_into, desc = "Step Into" },
-                { "<leader>dj", dap.down, desc = "Down" },
-                { "<leader>dk", dap.up, desc = "Up" },
-                { "<leader>dl", dap.run_last, desc = "Run Last" },
-                { "<leader>do", dap.step_out, desc = "Step Out" },
-                { "<leader>dO", dap.step_over, desc = "Step Over" },
-                { "<leader>dP", dap.pause, desc = "Pause" },
-                { "<leader>dr", dap.repl.toggle, desc = "Toggle REPL" },
-                { "<leader>ds", dap.session, desc = "Session" },
-                { "<leader>dt", dap.terminate, desc = "Terminate" },
-                {
-                    "<leader>dw",
-                    require("dap.ui.widgets").hover,
-                    desc = "Widgets",
-                },
-                { "<F5>", dap.continue, desc = "Debugger: Start" },
-                { "<F17>", dap.terminate, desc = "Debugger: Stop" }, -- Shift+F5
-                { "<F29>", dap.restart_frame, desc = "Debugger: Restart" }, -- Control+F5
-                { "<F6>", dap.pause, desc = "Debugger: Pause" },
-                { "<F7>", dap.reverse_continue, "Debugger: Reverse" },
-                { "<F10>", dap.step_over, desc = "Debugger: Step Over" },
-                { "<F11>", dap.step_into, desc = "Debugger: Step Into" },
-                { "<F23>", dap.step_out, desc = "Debugger: Step Out" }, -- Shift+F11
-            }
-            return vim.tbl_deep_extend("force", default_keys, keys)
-        end,
+        keys = {
+            {
+                "<leader>db",
+                function() require("dap").toggle_breakpoint() end,
+                desc = "Toggle Breakpoint",
+            },
+            {
+                "<leader>da",
+                function()
+                    require("persistent-breakpoints.api").load_breakpoints()
+                    require("dap").continue { before = get_args }
+                end,
+                desc = "Run with Args",
+            },
+            {
+                "<leader>dC",
+                function() require("dap").run_to_cursor() end,
+                desc = "Run to Cursor",
+            },
+            {
+                "<leader>dg",
+                function() require("dap").goto_() end,
+                desc = "Go to Line (No Execute)",
+            },
+            {
+                "<leader>di",
+                function() require("dap").step_into() end,
+                desc = "Step Into",
+            },
+            {
+                "<leader>dj",
+                function() require("dap").down() end,
+                desc = "Down",
+            },
+            { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+            {
+                "<leader>dl",
+                function() require("dap").run_last() end,
+                desc = "Run Last",
+            },
+            {
+                "<leader>do",
+                function() require("dap").step_out() end,
+                desc = "Step Out",
+            },
+            {
+                "<leader>dO",
+                function() require("dap").step_over() end,
+                desc = "Step Over",
+            },
+            {
+                "<leader>dP",
+                function() require("dap").pause() end,
+                desc = "Pause",
+            },
+            {
+                "<leader>dr",
+                function() require("dap").repl.toggle() end,
+                desc = "Toggle REPL",
+            },
+            {
+                "<leader>ds",
+                function() require("dap").session() end,
+                desc = "Session",
+            },
+            {
+                "<leader>dt",
+                function() require("dap").terminate() end,
+                desc = "Terminate",
+            },
+            {
+                "<leader>dw",
+                function() require("dap.ui.widgets").hover() end,
+                desc = "Widgets",
+            },
+            {
+                "<F5>",
+                function()
+                    require("persistent-breakpoints.api").load_breakpoints()
+                    require("dap").continue()
+                end,
+                desc = "Debugger: Start",
+            },
+            {
+                "<F17>",
+                function() require("dap").terminate() end,
+                desc = "Debugger: Stop",
+            }, -- Shift+F5
+            {
+                "<F29>",
+                function() require("dap").restart_frame() end,
+                desc = "Debugger: Restart",
+            }, -- Control+F5
+            {
+                "<F6>",
+                function() require("dap").pause() end,
+                desc = "Debugger: Pause",
+            },
+            {
+                "<F7>",
+                function() require("dap").reverse_continue() end,
+                "Debugger: Reverse",
+            },
+            {
+                "<F10>",
+                function() require("dap").step_over() end,
+                desc = "Debugger: Step Over",
+            },
+            {
+                "<F11>",
+                function() require("dap").step_into() end,
+                desc = "Debugger: Step Into",
+            },
+            {
+                "<F23>",
+                function() require("dap").step_out() end,
+                desc = "Debugger: Step Out",
+            }, -- Shift+F11
+        },
 
         config = function()
             -- add jsonc support to launch.json support
@@ -130,118 +303,22 @@ return {
             end
         end,
     },
-    {
-        "Weissle/persistent-breakpoints.nvim",
-        dependencies = {
-            "mfussenegger/nvim-dap",
-        },
-        event = { "BufReadPost" },
-        opts = {
-            load_breakpoints_event = { "BufReadPost" },
-        },
-        keys = function(_, keys)
-            local dap = require "dap"
-            local breakpoint = require "persistent-breakpoints.api"
-
-            local function edit_breakpoint()
-                -- Search for an existing breakpoint on this line in this buffer
-                ---@return dap.SourceBreakpoint bp that was either found, or an empty placeholder
-                local function find_bp()
-                    local buf_bps = require("dap.breakpoints").get(
-                        vim.fn.bufnr()
-                    )[vim.fn.bufnr()]
-                    ---@type dap.SourceBreakpoint
-                    local bp = {
-                        condition = "",
-                        logMessage = "",
-                        hitCondition = "",
-                        line = vim.fn.line ".",
-                    }
-                    for _, candidate in ipairs(buf_bps) do
-                        if
-                            candidate.line
-                            and candidate.line == vim.fn.line "."
-                        then
-                            bp = candidate
-                            break
-                        end
-                    end
-                    return bp
-                end
-
-                -- Elicit customization via a UI prompt
-                ---@param bp dap.SourceBreakpoint a breakpoint
-                local function customize_bp(bp)
-                    local props = {
-                        ["Condition"] = {
-                            value = bp.condition,
-                            setter = function(v) bp.condition = v end,
-                        },
-                        ["Hit Condition"] = {
-                            value = bp.hitCondition,
-                            setter = function(v) bp.hitCondition = v end,
-                        },
-                        ["Log Message"] = {
-                            value = bp.logMessage,
-                            setter = function(v) bp.logMessage = v end,
-                        },
-                    }
-                    local menu_options = {}
-                    for k, v in pairs(props) do
-                        table.insert(
-                            menu_options,
-                            ("%s: %s"):format(k, v.value)
-                        )
-                    end
-                    vim.ui.select(menu_options, {
-                        prompt = "Edit Breakpoint",
-                    }, function(choice)
-                        local prompt = (tostring(choice)):gsub(":.*", "")
-                        props[prompt].setter(vim.fn.input {
-                            prompt = prompt,
-                            default = props[prompt].value,
-                        })
-
-                        -- Set breakpoint for current line, with customizations (see h:dap.set_breakpoint())
-                        dap.set_breakpoint(
-                            bp.condition,
-                            bp.hitCondition,
-                            bp.logMessage
-                        )
-                    end)
-                end
-
-                customize_bp(find_bp())
-                breakpoint.breakpoints_changed_in_current_buffer()
-            end
-            local default_keys = {
-                {
-                    "<leader>dB",
-                    edit_breakpoint,
-                    desc = "Breakpoint Condition",
-                },
-                {
-                    "<F9>",
-                    breakpoint.toggle_breakpoint,
-                    desc = "Debugger: Toggle Breakpoint",
-                },
-                {
-                    "<F33>",
-                    breakpoint.clear_all_breakpoints,
-                    desc = "Debugger: Clear All Breakpoints",
-                },
-            }
-            return vim.tbl_deep_extend("force", default_keys, keys)
-        end,
-    },
 
     -- fancy UI for the debugger
     {
         "igorlfs/nvim-dap-view",
-        -- stylua: ignore
         keys = {
-          { "<leader>du", function() require("dap-view").toggle({ }) end, desc = "Dap UI" },
-          { "<leader>de", function() require("dap-view").add_expr() end, desc = "Watch", mode = {"n", "x"} },
+            {
+                "<leader>du",
+                function() require("dap-view").toggle() end,
+                desc = "Dap UI",
+            },
+            {
+                "<leader>de",
+                function() require("dap-view").add_expr() end,
+                desc = "Watch",
+                mode = { "n", "x" },
+            },
         },
         opts = {
             windows = {
@@ -251,15 +328,6 @@ return {
                 },
             },
             winbar = {
-                sections = {
-                    "watches",
-                    "scopes",
-                    "exceptions",
-                    "breakpoints",
-                    "disassembly",
-                    "threads",
-                    "repl",
-                },
                 controls = {
                     enabled = true,
                     buttons = {
@@ -312,10 +380,5 @@ return {
                 dapview.open()
             end
         end,
-    },
-    {
-        "Jorenar/nvim-dap-disasm",
-        dependencies = "igorlfs/nvim-dap-view",
-        opts = {},
     },
 }
