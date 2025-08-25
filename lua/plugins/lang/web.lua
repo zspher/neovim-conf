9a99701794244adec3a10e829195b426881383c5
zspher <66728045+zspher@users.noreply.github.com>
2025-08-25 13:02:48 -0600

    feat: move lang/web - css, html

---
+++ lua/plugins/lang/web.lua
@@ -0,0 +1,60 @@
+---@type LazySpec[]
+return {
+
+  -- lsp
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      ---@type table<string, vim.lsp.Config>
+      servers = {
+        -- css
+        cssmodules_ls = {},
+        cssls = {
+          settings = {
+            css = { lint = { unknownAtRules = "ignore" } },
+          },
+        },
+        tailwindcss = {},
+
+        -- html
+        superhtml = {},
+        html = {
+          settings = {
+            html = { format = { indentInnerHtml = true } },
+          },
+        },
+        emmet_language_server = {},
+
+        -- general
+        biome = {
+          single_file_support = true,
+        },
+      },
+    },
+  },
+  -- formatter
+
+  -- linter
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        html = { "markuplint" },
+      },
+    },
+  },
+
+  -- syntax highlight
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "css", "scss", "html" },
+    },
+  },
+
+  -- test suite
+
+  -- dap
+
+  -- extra
+}
