96a06b2ee004624abebb205e9a753f902de39470
zspher <66728045+zspher@users.noreply.github.com>
2025-08-24 18:36:43 -0600

    feat: move lang/sql

---
+++ lua/plugins/lang/sql.lua
@@ -0,0 +1,107 @@
+---@type LazySpec[]
+return {
+
+    -- lsp
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            ---@type table<string, vim.lsp.Config>
+            servers = {
+                bashls = {},
+            },
+        },
+    },
+    -- formatter
+    {
+        "stevearc/conform.nvim",
+        optional = true,
+        opts = {
+            formatters = {
+                sqlfluff = {
+                    args = { "format", "--dialect=ansi", "-" },
+                    require_cwd = false,
+                },
+            },
+            formatters_by_ft = {
+                sql = { "sqlfluff" },
+                mysql = { "sqlfluff" },
+                plsql = { "sqlfluff" },
+            },
+        },
+    },
+    -- linter
+    {
+        "mfussenegger/nvim-lint",
+        optional = true,
+        opts = {
+            linters_by_ft = {
+                sql = { "sqlfluff" },
+                mysql = { "sqlfluff" },
+                plsql = { "sqlfluff" },
+            },
+        },
+    },
+
+    -- syntax highlight
+    {
+        "nvim-treesitter/nvim-treesitter",
+        optional = true,
+        opts = { ensure_installed = { "sql" } },
+    },
+
+    -- test suite
+
+    -- dap
+
+    -- extra
+    {
+        "saghen/blink.cmp",
+        optional = true,
+        opts = {
+            sources = {
+                default = { "dadbod" },
+                providers = {
+                    dadbod = {
+                        name = "Dadbod",
+                        module = "vim_dadbod_completion.blink",
+                    },
+                },
+            },
+        },
+        dependencies = {
+            "kristijanhusak/vim-dadbod-completion",
+        },
+    },
+    {
+        "tpope/vim-dadbod",
+        cmd = "DB",
+    },
+    {
+        "kristijanhusak/vim-dadbod-completion",
+        dependencies = "vim-dadbod",
+        ft = { "sql", "plsql", "mysql" },
+    },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+        dependencies = "vim-dadbod",
+        keys = {
+            { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+        },
+        init = function()
+            local data_path = vim.fn.stdpath "data"
+
+            vim.g.db_ui_save_location = vim.uv.cwd()
+            vim.g.db_ui_show_database_icon = true
+            vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+            vim.g.db_ui_use_nerd_fonts = true
+            vim.g.db_ui_use_nvim_notify = true
+
+            -- NOTE: The default behavior of auto-execution of queries on save is disabled
+            -- this is useful when you have a big query that you don't want to run every time
+            -- you save the file running those queries can crash neovim to run use the
+            -- default keymap: <leader>S
+            vim.g.db_ui_execute_on_save = false
+        end,
+    },
+}
