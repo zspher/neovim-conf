091b6f035128ec4d36f9c3de0a558a0f3b8f1d9e
zspher <66728045+zspher@users.noreply.github.com>
2025-09-17 18:57:08 -0600

    feat: update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,63 +1,50 @@
 ---@type LazySpec[]
 return {
   {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { "<BS>", desc = "Decrement Selection", mode = "x" },
-        {
-          "<c-space>",
-          desc = "Increment Selection",
-          mode = { "x", "n" },
-        },
-      },
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
+    branch = "main",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
-    main = "nvim-treesitter.configs",
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require "nvim-treesitter.query_predicates"
-    end,
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<BS>", desc = "Decrement Selection", mode = "x" },
-    },
+    cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
     opts_extend = { "ensure_installed" },
-    ---@type TSConfig
     opts = {
-      auto_install = false,
-      modules = {},
-      sync_install = false,
-      ignore_install = {},
       ensure_installed = {
         "diff",
         "query",
         "vim",
         "vimdoc",
       },
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
     },
+    config = function(_, opts)
+      local TS = require "nvim-treesitter"
+      TS.setup(opts)
+
+      local installed = {}
+      for _, lang in ipairs(require("nvim-treesitter").get_installed "parsers") do
+        installed[lang] = lang
+      end
+
+      local to_install = vim.tbl_filter(function(ft)
+        local lang = vim.treesitter.language.get_lang(ft)
+        return not (lang and installed[lang])
+      end, opts.ensure_installed)
+      if #to_install > 0 then TS.install(to_install, { summary = true }) end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local bufnr = args.buf
+          local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+          if not ok or not parser then return end
+          pcall(vim.treesitter.start)
+
+          local ft = vim.bo[bufnr].filetype
+          for _, value in ipairs(opts.ensure_installed) do
+            if ft == value then vim.bo[bufnr].syntax = "on" end
+          end
+        end,
+      })
+    end,
   },
 }
