1c81c247b00c1267724df754cd8bc4f1d45eebef
zspher <66728045+zspher@users.noreply.github.com>
2026-01-05 14:29:22 -0700

    feat(rust): remove `mason` stuff

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -74,17 +74,6 @@ return {
       },
     },
     config = function(_, opts)
-      local codelldb = vim.fn.exepath "codelldb"
-      local codelldb_lib_ext = io.popen("uname"):read "*l" == "Linux" and ".so"
-        or ".dylib"
-      local library_path =
-        vim.fn.expand("$MASON/opt/lldb/lib/liblldb" .. codelldb_lib_ext)
-      opts.dap = {
-        adapter = require("rustaceanvim.config").get_codelldb_adapter(
-          codelldb,
-          library_path
-        ),
-      }
       vim.g.rustaceanvim =
         vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
     end,
