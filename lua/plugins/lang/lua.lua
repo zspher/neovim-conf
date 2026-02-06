8711d402828aaaaebc5dad74429b16eec92d4f0b
zspher <66728045+zspher@users.noreply.github.com>
2026-02-06 10:43:57 -0700

    fix: lazydev librabries not imported/known by lua_ls on startup

--- lua/plugins/lang/lua.lua
+++ lua/plugins/lang/lua.lua
@@ -11,6 +11,11 @@ return {
             Lua = {
               workspace = {
                 checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                  { path = "lazy.nvim", words = { "LazyVim" } },
+                },
               },
               codeLens = {
                 enable = true,
