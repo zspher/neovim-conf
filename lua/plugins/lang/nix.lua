8dfe85326f6f6d68a5ef042be2a193715564ba66
zspher <66728045+zspher@users.noreply.github.com>
2026-02-20 00:15:43 -0700

    feat(nix): remove nil
    - nixd now has code action support

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -39,24 +39,6 @@ return {
             },
           },
         },
-        -- for code actions and goto file (e.g. ./package/nixft)
-        nil_ls = {
-          on_attach = function(client, _)
-            client.server_capabilities.semanticTokensProvider = nil
-            client.server_capabilities.diagnosticProvider = nil
-            client.server_capabilities.documentSymbolProvider = nil
-            client.server_capabilities.documentFormattingProvider = nil
-            client.server_capabilities.renameProvider = nil
-            client.server_capabilities.completionProvider = nil
-            client.server_capabilities.hoverProvider = nil
-          end,
-          settings = {
-            ["nil"] = {
-              formatting = { command = { "" } },
-              nix = { flake = { nixpkgsInputName = "" } },
-            },
-          },
-        },
       },
     },
   },
