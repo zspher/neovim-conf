28efe6905856347b8291475b7d57d093c1c2c3c7
zspher <66728045+zspher@users.noreply.github.com>
2026-05-08 14:25:27 -0600

    feat: remove some keymaps

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -445,17 +445,6 @@ return {
     },
     keys = {
       {
-        "<leader>n",
-        function()
-          if Snacks.config.picker and Snacks.config.picker.enabled then
-            Snacks.picker.notifications()
-          else
-            Snacks.notifier.show_history()
-          end
-        end,
-        desc = "Notification History",
-      },
-      {
         "<leader>un",
         function() Snacks.notifier.hide() end,
         desc = "Dismiss All Notifications",
