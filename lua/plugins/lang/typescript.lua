4f7103afcd7c68604be3f9051929743db3e5894c
zspher <66728045+zspher@users.noreply.github.com>
2026-05-13 17:56:05 -0600

    feat(typescript): add dap package.json `main` & `bin` support

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -72,6 +72,46 @@ return {
       local js_filetypes =
         { "typescript", "javascript", "typescriptreact", "javascriptreact" }
 
+      local function get_program()
+        return coroutine.create(function(dap_run_co)
+          local cwd = vim.uv.cwd()
+          local items = { [vim.api.nvim_buf_get_name(0)] = true }
+
+          local package_path = vim.fs.normalize(cwd .. "/package.json")
+          local fd = vim.uv.fs_open(package_path, "r", 438)
+          if fd then
+            local stat = vim.uv.fs_fstat(fd)
+            local raw = stat and vim.uv.fs_read(fd, stat.size, 0)
+            vim.uv.fs_close(fd)
+            local ok, data = pcall(vim.json.decode, raw or "{}")
+
+            if ok and data then
+              local bins = data.main and { data.main } or {}
+              vim.list_extend(
+                bins,
+                type(data.bin) == "table" and data.bin or { data.bin }
+              )
+              for _, bin in ipairs(bins) do
+                local path = vim.fs.normalize(cwd .. "/" .. bin)
+                local f = vim.uv.fs_stat(path)
+                if f and f.type == "file" then items[path] = true end
+              end
+            end
+          end
+
+          items = vim.tbl_keys(items)
+
+          if #items == 1 then
+            coroutine.resume(dap_run_co, items[1])
+            return
+          end
+          vim.ui.select(items, {
+            format_item = function(path) return vim.fn.fnamemodify(path, ":.") end,
+          }, function(choice)
+            if choice then coroutine.resume(dap_run_co, choice) end
+          end)
+        end)
+      end
       for _, language in ipairs(js_filetypes) do
         if not dap.configurations[language] then
           dap.configurations[language] = {
@@ -79,7 +119,7 @@ return {
               type = "node",
               request = "launch",
               name = "Launch file",
-              program = "${file}",
+              program = get_program,
               cwd = "${workspaceFolder}",
               console = "integratedTerminal",
             },
