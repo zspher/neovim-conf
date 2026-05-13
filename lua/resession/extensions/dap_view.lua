a791f39b51bef7a3c63dfee1d35a1cf9ee1b74c5
zspher <66728045+zspher@users.noreply.github.com>
2026-05-13 14:35:28 -0600

    feat(resession): use nvim-dap-view to save exception breakpoints

--- lua/resession/extensions/dap_view.lua
+++ lua/resession/extensions/dap_view.lua
@@ -7,11 +7,11 @@ M.on_save = function(opts)
   if not package.loaded["dap-view"] then return nil end
   local state = require "dap-view.state"
 
-  if vim.tbl_isempty(state.watched_expressions) then return nil end
   return {
     section = state.current_section,
     expr_count = state.expr_count,
     watches = state.watched_expressions,
+    exceptions_options = state.exceptions_options,
   }
 end
 
@@ -23,6 +23,10 @@ M.on_post_load = function(data)
     state.current_section = data.section or ""
     state.expr_count = data.expr_count or 0
     state.watched_expressions = data.watches or {}
+
+    -- HACK: stop gap solution to saving breakpoints
+    --       probably should be implemented in nvim-dap
+    state.exceptions_options = data.exceptions_options or {}
   end
 end
 
