529e3370033213e7acdff3ddfc2507ca432f0ebe
zspher <66728045+zspher@users.noreply.github.com>
2026-05-18 18:43:14 -0600

    feat(blink.cmp): removed default opts

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -214,16 +214,6 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      snippets = {},
-      appearance = {
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
-        use_nvim_cmp_as_default = false,
-        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
       completion = {
         menu = {
           draw = {
@@ -239,32 +229,17 @@ return {
             },
           },
         },
-        accept = {
-          -- experimental auto-brackets support
-          auto_brackets = {
-            enabled = true,
-          },
-        },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 200,
         },
-        ghost_text = {
-          enabled = true,
-        },
+        ghost_text = { enabled = true },
       },
-
-      -- experimental signature help support
-      -- signature = { enabled = true },
-
       sources = {
         -- adding any nvim-cmp sources here will enable them
         -- with blink.compat
         compat = {},
         default = { "lsp", "path", "snippets", "buffer" },
-      },
-      cmdline = {
-        enabled = true,
       },
       keymap = {
         preset = "default",
