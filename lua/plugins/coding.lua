ffee48d0d727408d9902d3bbf27d818ca13fd26d
zspher <66728045+zspher@users.noreply.github.com>
2026-05-21 10:14:52 -0600

    feat: remove redundant code

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -53,7 +53,6 @@ return {
       {
         "rafamadriz/friendly-snippets",
         config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
           require("luasnip.loaders.from_vscode").lazy_load {
             paths = { vim.fn.stdpath "config" .. "/snippets" },
           }
