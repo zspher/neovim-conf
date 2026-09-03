8d6383ffdf5cd3d978802036e6545d289b05219e
zspher <66728045+zspher@users.noreply.github.com>
2026-09-03 00:14:16 -0600

    feat: add java lang

---
+++ lua/plugins/lang/java.lua
@@ -0,0 +1,65 @@
+---@type LazySpec[]
+return {
+  -- lsp
+  {
+    "nvim-java/nvim-java",
+    ft = { "java" },
+    ---@module "java"
+    ---@type java.PartialConfig
+    opts = {
+      java_test = {
+        path = vim.fn.stdpath "data" .. "/java/test",
+      },
+      java_debug_adapter = {
+        path = vim.fn.stdpath "data" .. "/java/debug",
+      },
+      spring_boot_tools = {
+        enable = false,
+      },
+      lombok = {
+        enable = false,
+        path = vim.fn.stdpath "data" .. "/java/lombok.jar",
+      },
+      jdk = {
+        auto_install = false,
+      },
+    },
+    config = function(_, opts)
+      require("java").setup(opts)
+      vim.lsp.enable "jdtls"
+    end,
+  },
+  -- formatter
+
+  -- linter
+
+  -- syntax highlight
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "java" },
+    },
+  },
+
+  -- test suite
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "rcasia/neotest-java",
+    },
+    opts = {
+      adapters = {
+        ["neotest-java"] = {},
+      },
+    },
+  },
+
+  -- dap
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+  },
+
+  -- extra
+}
